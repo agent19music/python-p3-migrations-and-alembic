d523022cba91f9de3fe4eea98d0e4cad58930ee1
agent19music <seanmotanya@gmail.com>
2023-12-14 06:09:30 +0300

    Done

---
+++ app/migrations/versions/7af40cc3cec9_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 7af40cc3cec9
+Revises: 
+Create Date: 2023-12-14 05:52:41.041919
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7af40cc3cec9'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
